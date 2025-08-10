a9fa528bbc9fce614ac339e63ee13f84b3904c96
Malahit <138647015+Malahit@users.noreply.github.com>
2025-08-10 11:33:59 +0300

    Create env.ts

---
+++ api/src/env.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+
+export const env = {
+  PORT: parseInt(process.env.PORT || '3000', 10),
+  DATABASE_URL: process.env.DATABASE_URL || '',
+  JWT_SECRET: process.env.JWT_SECRET || 'PLEASE_SET_JWT_SECRET',
+  POSTHOG_API_KEY: process.env.POSTHOG_API_KEY || '',
+  POSTHOG_HOST: process.env.POSTHOG_HOST || 'https://app.posthog.com'
+};
