c30b3034b960433ad7e64c3b0685b2ef7e260cf1
Malahit <138647015+Malahit@users.noreply.github.com>
2025-08-10 13:13:37 +0300

    Update App.tsx

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -2,12 +2,13 @@ import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigator from './navigation';
 import PostHog from 'posthog-react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
   useEffect(() => {
-    // TODO: заменить ключ на реальный
-    PostHog.init(process.env.EXPO_PUBLIC_POSTHOG_API_KEY || 'PHC_XXXX', {
-      host: process.env.EXPO_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+    const extra: any = Constants.expoConfig?.extra || {};
+    PostHog.init(extra.POSTHOG_API_KEY || 'PHC_XXXX', {
+      host: extra.POSTHOG_HOST || 'https://app.posthog.com',
       captureApplicationLifecycleEvents: true
     });
   }, []);
